7025369371c6ce8e1acd405ef4c6bf3254765b3a
Cristiano-Brito <62959941+Cristiano-Brito@users.noreply.github.com>
2021-02-22 22:39:55 -0300

    Configuração do ambiente e criação de rota GET e POST.

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express, { request, response } from 'express';
+
+const app = express();
+
+/**
+ * GET => Busca
+ * POST => Salvar
+ * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração específica
+ */
+
+ app.get("/", (request, response) => {
+     return response.json({ message: "Hello World - NLW04" });
+ });
+
+ // param => Rora(Recurso API)
+ // param => request, response
+
+ app.post("/", (request, response) =>{
+     //Recebeu os dados para salvar
+     return response.json({ message: "Os dados foram salvos com sucesso!" });
+ });
+
+app.listen(3333, () => console.log("Server is running!"));
