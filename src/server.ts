f91c9d25cfae708d2e4a6949d985c26666702078
Cristiano-Brito <62959941+Cristiano-Brito@users.noreply.github.com>
2021-02-23 21:14:42 -0300

    Criação do banco de dados, migration, model, controller, rota e configuração do TypeORM.

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,11 @@
+import "reflect-metadata";
 import express, { request, response } from 'express';
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-/**
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração específica
- */
-
- app.get("/", (request, response) => {
-     return response.json({ message: "Hello World - NLW04" });
- });
-
- // param => Rora(Recurso API)
- // param => request, response
-
- app.post("/", (request, response) =>{
-     //Recebeu os dados para salvar
-     return response.json({ message: "Os dados foram salvos com sucesso!" });
- });
+app.use(express.json());
+app.use(router)
 
 app.listen(3333, () => console.log("Server is running!"));
